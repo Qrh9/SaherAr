a34ef9b3d249d9849e6b45ffb78974baa8ae3e1a
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-07-05 00:06:48 +0300

    2

--- SHRU/assistant/start.py
+++ SHRU/assistant/start.py
@@ -51,7 +51,7 @@ async def start(event):
             message=starttext,
             link_preview=False,
             buttons=[
-                [custom.Button.inline("تنـصيب جيـبثون  🐍", data="deploy")],
+                [custom.Button.inline("تنـصيب الساحر  🐍", data="deploy")],
                 [Button.url("تحتاج مسـاعدة ❓", "https://t.me/SX9OO")],
             ],
         )
