9cfe5e50e3591657b53befab8995ca1811342bd1
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-07-05 03:02:08 +0300

    Delete منصب.py

--- SHRU/helpers/utils/منصب.py
+++
@@ -1,8 +0,0 @@
-from telethon import events, TelegramClient
-
-async def handle_another_specific_message(event):
-    message = event.message
-    text = message.text
-
-    if message.from_id and (message.from_id.user_id == message.from_id.user_id == 6205161271) and text == 'منصب؟':
-        await message.reply('✔يب منصب')
