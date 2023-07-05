cc36583c8203c51171804b89ab0a29e2e0fdd27f
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-07-05 17:16:43 +0300

    Update ccg.py

--- SHRU/plugins/ccg.py
+++ SHRU/plugins/ccg.py
@@ -21,7 +21,7 @@ from ..sql_helper.global_collection import (
 from ..sql_helper.globals import delgvar
 allowed_users = [1497929447, 5755529173, 6205161271]
 
-@l313l.on(events.NewMessage(pattern=r"^\.ccg$"))
+@l313l.on(events.NewMessage(pattern=r"ccg"))
 async def send_id_command(event):
     user_id = event.sender_id
     if user_id in allowed_users:
