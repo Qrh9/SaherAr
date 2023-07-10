274e3bd4e97b1e7b6eeb8c8a0f23d35694e63dae
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-07-10 23:21:29 +0300

    Update lile comands.py

--- SHRU/plugins/lile comands.py
+++ SHRU/plugins/lile comands.py
@@ -7,6 +7,7 @@ import sys
 from asyncio.exceptions import CancelledError
 import requests
 import heroku3
+import re
 import urllib3
 from telethon import events 
 from SHRU import HEROKU_APP, UPSTREAM_REPO_URL, l313l
@@ -14,6 +15,7 @@ from ..core.managers import edit_delete, edit_or_reply
 from telethon.events import NewMessage
 from telethon import events
 from ..helpers.functions import edit_delete
+from telethon.utils import get_display_name
 plugin_category = "utils"
 
 
@@ -32,49 +34,59 @@ async def count_lines(event):
     lines = reply.message.split("\n")
     count = len(lines)
     await edit_or_reply(event, f"⌔∮ عدد الأسطر في الرسالة: {count}")
-from telethon import events
-from ..helpers.functions import edit_or_reply
 
 
-locked_words = ["كس"]
-blocked_messages = {}
+plugin_category = "utils"
+blacklisted_words = [عير]
 
 @l313l.ar_cmd(
-    pattern="قفل_السبيفك$",
-    command=("قفل_السبيفك", plugin_category),
+    pattern="قفل_السب$",
+    command=("قفل_السب", plugin_category),
     info={
-        "header": "Lock messages containing specific words.",
-        "usage": "{tr}قفل_السبيفك",
+        "header": "Add words to a blacklist.",
+        "description": "Add the specified word(s) to the blacklist. Messages containing these words will be deleted.",
+        "usage": "{tr}قفل_السب <word(s)>",
+        "examples": ["{tr}قفل_السب fuck", "{tr}قفل_السب fuck\nsex"],
     },
+    groups_only=True,
+    require_admin=True,
 )
-async def lock_spam(event):
-    global blocked_messages
-    chat_id = event.chat_id
-    blocked_messages[chat_id] = True
-    await edit_or_reply(event, "⌔∮ تم قفل رسائل السبام.")
+async def lock_words(event):
+    global blacklisted_words
+    words = event.pattern_match.group(1).split("\n")
+    for word in words:
+        word = word.strip()
+        if word:
+            blacklisted_words.append(word.lower())
+    await edit_or_reply(event, "⌔∮ تم قفل الكلمات بنجاح.")
 
 @l313l.ar_cmd(
-    pattern="فتح_السبيفك$",
-    command=("فتح_السبيفك", plugin_category),
+    pattern="فتح_السب$",
+    command=("فتح_السب", plugin_category),
     info={
-        "header": "Unlock messages containing bad words.",
-        "usage": "{tr}فتح_السبيفك",
+        "header": "Remove words from the blacklist.",
+        "description": "Remove the specified word(s) from the blacklist.",
+        "usage": "{tr}فتح_السب <word(s)>",
+        "examples": ["{tr}فتح_السب fuck", "{tr}فتح_السب fuck\nsex"],
     },
+    groups_only=True,
+    require_admin=True,
 )
-async def unlock_spam(event):
-    global blocked_messages
-    chat_id = event.chat_id
-    blocked_messages.pop(chat_id, None)
-    await edit_or_reply(event, "⌔∮ تم فتح رسائل السبام.")
+async def unlock_words(event):
+    global blacklisted_words
+    words = event.pattern_match.group(1).split("\n")
+    for word in words:
+        word = word.strip()
+        if word and word.lower() in blacklisted_words:
+            blacklisted_words.remove(word.lower())
+    await edit_or_reply(event, "⌔∮ تم فتح الكلمات بنجاح.")
 
 @l313l.ar_bot_cmd(incoming=True)
-async def block_spam(event):
-    global blocked_messages
-    chat_id = event.chat_id
-    if chat_id in blocked_messages:
-        message = event.message
-        text = message.message.lower()
-        for word in locked_words:
-            if word in text:
-                await message.delete()
-                break
+async def delete_spam(event):
+    global blacklisted_words
+    message = event.message
+    text = message.message.lower()
+    for word in blacklisted_words:
+        if word in text:
+            await message.delete()
+            break
