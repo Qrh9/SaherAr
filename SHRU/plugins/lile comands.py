b6af16c7e441dc0b6c4e25627aafe83c38ec2d84
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-07-10 22:54:15 +0300

    Update lile comands.py

--- SHRU/plugins/lile comands.py
+++ SHRU/plugins/lile comands.py
@@ -13,6 +13,7 @@ from SHRU import HEROKU_APP, UPSTREAM_REPO_URL, l313l
 from ..core.managers import edit_delete, edit_or_reply
 from telethon.events import NewMessage
 from telethon.tl.types import ChatBannedRights
+
 plugin_category = "utils"
 
 
@@ -32,58 +33,79 @@ async def count_lines(event):
     count = len(lines)
     await edit_or_reply(event, f"⌔∮ عدد الأسطر في الرسالة: {count}")
 
-
-from telethon import events
+from telethon.tl.types import ChatBannedRights
 
 @l313l.ar_cmd(
     pattern="قفل_السب$",
     command=("قفل_السب", plugin_category),
     info={
-        "header": "قفل الرسائل التي تحتوي على كلمات غير مرغوب فيها.",
+        "header": "لقفل وحذف الرسائل التي تحتوي على كلمات سيئة.",
         "usage": "{tr}قفل_السب",
     },
     groups_only=True,
     require_admin=True,
 )
 async def lock_bad_words(event):
+    chat_id = event.chat_id
+    banned_rights = ChatBannedRights(
+        until_date=None,
+        view_messages=True,
+        send_messages=True,
+        send_media=True,
+        send_stickers=True,
+        send_gifs=True,
+        send_games=True,
+        send_inline=True,
+        send_polls=True,
+        invite_users=True,
+        pin_messages=True,
+        change_info=True,
+    )
     lock_words = ["كسمك", "فرخ", "نيج"]
-    await event.edit("تم تشغيل وظيفة قفل السب.")
 
-    @events.register(events.NewMessage(incoming=True, chats=event.chat_id))
     async def lock_and_delete_messages(event):
-        if event.sender_id == event.client.uid or not event.is_group:
-            return
-
         if any(word.lower() in event.raw_text.lower() for word in lock_words):
             await event.delete()
 
-    await event.client.add_event_handler(lock_and_delete_messages)
+    event.client.add_event_handler(
+        lock_and_delete_messages, events.NewMessage(incoming=True, chats=[chat_id])
+    )
+    await event.client.edit_permissions(chat_id, event.client.uid, banned_rights)
+    await event.edit("تـم قفل السب بنجـاح!")
 
 
 @l313l.ar_cmd(
     pattern="فتح_السب$",
     command=("فتح_السب", plugin_category),
     info={
-        "header": "إلغاء قفل الرسائل التي تحتوي على كلمات غير مرغوب فيها.",
+        "header": "لفتح السب والسماح بإرسال الرسائل السيئة.",
         "usage": "{tr}فتح_السب",
     },
     groups_only=True,
     require_admin=True,
 )
 async def unlock_bad_words(event):
-    await event.edit("تم إلغاء قفل السب.")
-    await event.client.remove_event_handler(lock_and_delete_messages)
+    chat_id = event.chat_id
+    banned_rights = ChatBannedRights(
+        until_date=None,
+        view_messages=True,
+        send_messages=False,
+        send_media=False,
+        send_stickers=False,
+        send_gifs=False,
+        send_games=False,
+        send_inline=False,
+        send_polls=False,
+        invite_users=False,
+        pin_messages=False,
+        change_info=False,
+    )
 
+    async def lock_and_delete_messages(event):
+        pass
 
-async def lock_and_delete_messages(event):
-    if event.is_group and event.text.startswith(".قفل_السب"):
-        lock_words = ["كسمك", "فرخ", "نيج"]
-        sender_id = event.sender_id
-
-        if not await event.client.is_chat_admin(event.chat_id, sender_id):
-            await event.reply("ليس لديك إذن بحذف الرسائل.")
-            return
-
-        async for message in event.client.iter_messages(event.chat_id):
-            if message.sender_id != event.client.uid and any(word.lower() in message.raw_text.lower() for word in lock_words):
-                await message.delete()
+    event.client.remove_event_handler(
+        lock_and_delete_messages, events.NewMessage(incoming=True, chats=[chat_id])
+    )
+    await event.client.edit_permissions(chat_id, event.client.uid, banned_rights)
+    await event.edit("تـم فتح السب بنجاح!")
