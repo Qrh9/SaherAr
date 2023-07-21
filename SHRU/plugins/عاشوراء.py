dcb589907e468c5ef3f56a52313dc097132c5401
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-07-21 03:36:04 +0300

    Update عاشوراء.py

--- SHRU/plugins/عاشوراء.py
+++ SHRU/plugins/عاشوراء.py
@@ -1,8 +1,23 @@
-from SHRU import l313l
-
+import asyncio
+import os
+import contextlib
 import random
-
-from ..helpers.functions import edit_or_reply
+import sys
+from asyncio.exceptions import CancelledError
+import requests
+import heroku3
+import urllib3
+from telethon import events 
+from SHRU import HEROKU_APP, UPSTREAM_REPO_URL, l313l
+from ..Config import Config
+from ..core.logger import logging
+from ..core.managers import edit_delete, edit_or_reply
+from ..sql_helper.global_collection import (
+    add_to_collectionlist,
+    del_keyword_collectionlist,
+    get_collectionlist_items,
+)
+from ..sql_helper.globals import delgvar
 plugin_category = "utils"
 ashour = [
     "أَقِيمُوا الْعَزَاء اُذْكُرُوا سِّيِّد الشَّبَاب قتييل كَرْبَلا ءاِبْن عَلِيّ والزهراء",
