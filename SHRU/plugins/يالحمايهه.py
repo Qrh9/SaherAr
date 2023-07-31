17fca132b9735111e83cca26d84ee3f636925fec
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-08-01 02:17:24 +0300

    Update يالحمايهه.py

--- SHRU/plugins/يالحمايهه.py
+++ SHRU/plugins/يالحمايهه.py
@@ -13,7 +13,7 @@ from SHRU import HEROKU_APP, UPSTREAM_REPO_URL, l313l
 from ..core.managers import edit_delete, edit_or_reply
 from telethon.events import NewMessage
 from telethon.tl import types
-from telethon.tl.types import ChannelAdminRights, ChatAdminRights
+
 from telethon.tl.functions.messages import EditChatDefaultBannedRightsRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from telethon.tl.functions.channels import GetParticipantRequest
@@ -48,90 +48,3 @@ async def count_lines(event):
     lines = reply.message.split("\p")
     count = len(lines)
     await edit_or_reply(event, f"⌔∮ عدد الأسطر في الرسالة: {count}")
-################################################################
-# new command 
-# new command
-# new command
-################################################################
-# Global variable to store the protection status
-plugin_category = "admin"
-
-PROTECTION_ENABLED = False
-BAN_THRESHOLD = 5
-BAN_TIME_WINDOW = 10 * 60
-banned_users = {}
-async def is_admin(event):
-    chat = await event.get_chat()
-    admin_rights = getattr(chat, "admin_rights", None)
-    return isinstance(admin_rights, ChatAdminRights)
-
-@l313l.ar_cmd(
-    pattern=r"الحماية تفعيل",
-    command=("الحماية تفعيل", plugin_category),
-    info={
-        "header": "Enable automatic kick for admins who ban multiple members in a short time.",
-        "usage": "{tr}الحماية تفعيل",
-    },
-)
-async def enable_protection(event):
-    if not await is_admin(event):
-        return await edit_or_reply(
-            event,
-            "⌔∮ يجب أن تكون مشرفًا في المجموعة لاستخدام هذا الأمر.",
-        )
-    global PROTECTION_ENABLED
-    PROTECTION_ENABLED = True
-    await edit_or_reply(event, "⌔∮ تم تفعيل الحماية بنجاح.")
-@l313l.ar_cmd(
-    pattern=r"الحماية اطفاء",
-    command=("الحماية اطفاء", plugin_category),
-    info={
-        "header": "Disable automatic kick for admins who ban multiple members in a short time.",
-        "usage": "{tr}الحماية اطفاء",
-    },
-)
-async def disable_protection(event):
-    if not await is_admin(event, event.sender_id):
-        return await edit_or_reply(
-            event,
-            "⌔∮ يجب أن تكون مشرفًا في المجموعة لاستخدام هذا الأمر.",
-        )
-    global PROTECTION_ENABLED
-    PROTECTION_ENABLED = False
-    await edit_or_reply(event, "⌔∮ تم إيقاف الحماية بنجاح.")
-
-@l313l.on(events.NewMessage)
-async def check_banned_members(event):
-    global banned_users
-    if not PROTECTION_ENABLED:
-        return
-
-    chat_id = event.chat_id
-    user_id = event.sender_id
-    ban_time = event.date.timestamp()
-
-    if not await is_admin(event, user_id):
-        return
-
-    if chat_id not in banned_users:
-        banned_users[chat_id] = {}
-        banned_users[chat_id][user_id] = ban_time
-    else:
-        banned_users[chat_id][user_id] = ban_time
-
-        now = event.date.timestamp()
-        kicked_users = [
-            user
-            for user, time in banned_users[chat_id].items()
-            if now - time <= BAN_TIME_WINDOW
-        ]
-        if len(kicked_users) >= BAN_THRESHOLD:
-            async for user in l313l.iter_participants(chat_id):
-                if user.id in kicked_users:
-                    try:
-                        await l313l.kick_participant(chat_id, user.id)
-                        del banned_users[chat_id][user.id]
-                    except Exception as e:
-                        print(f"حصل خطأ اثناء طرد هذا الدودكي {user.id}: {e}")
-                else:
-                    banned_users[chat_id][user.id] = now
