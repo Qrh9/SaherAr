8624cc8ed6dba3cbbd39064f3d822c0b90e26676
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-08-01 02:58:05 +0300

    Update يالحمايهه.py

--- SHRU/plugins/يالحمايهه.py
+++ SHRU/plugins/يالحمايهه.py
@@ -1,50 +1,46 @@
-
-import asyncio
-import os
-import contextlib
-import random
-import sys
-from asyncio.exceptions import CancelledError
-import requests
-import heroku3
-import re
-import urllib3
-from SHRU import HEROKU_APP, UPSTREAM_REPO_URL, l313l
-from ..core.managers import edit_delete, edit_or_reply
-from telethon.events import NewMessage
+import time
 from telethon.tl import types
-
-from telethon.tl.functions.messages import EditChatDefaultBannedRightsRequest
-from telethon.tl.functions.messages import ImportChatInviteRequest as Get
-from telethon.tl.functions.channels import GetParticipantRequest
-from telethon.utils import get_display_name
 from telethon import events
-from telethon.tl.functions.channels import EditBannedRequest
-from telethon.tl.types import ChannelParticipantAdmin
-from telethon.errors import UserNotParticipantError, PeerIdInvalidError
-
-from telethon.tl.types import Channel , ChatBannedRights
-plugin_category = "admin"
 from SHRU import l313l
+# Global variable to keep track of banned members count and time
+banned_count = {}
+ban_time = 60  # 60 seconds
 
-from ..core.managers import edit_delete, edit_or_reply
-from ..helpers.utils import _format
-from ..sql_helper.locks_sql import get_locks, is_locked, update_lock
-from ..utils import is_admin
-from . import BOTLOG, get_user_from_event
+# Function to check if an admin should be banned
+def should_ban_admin(chat_id, admin_id):
+    if admin_id in banned_count:
+        current_time = time.time()
+        last_ban_time = banned_count[admin_id]
+        if current_time - last_ban_time <= ban_time:
+            return True
+        else:
+            banned_count[admin_id] = current_time
+    else:
+        banned_count[admin_id] = time.time()
+    return False
 
-@l313l.ar_cmd(
-    pattern="عدد$",
-    command=("عدد", plugin_category),
-    info={
-        "header": "Count the number of lines in a message.",
-        "usage": "{tr}عدد (reply to a message)",
-    },
-)
-async def count_lines(event):
-    reply = await event.get_reply_message()
-    if not reply or not reply.message:
-        return await edit_or_reply(event, "⌔∮ يرجى الرد على الرسالة لحساب عدد الأسطر.")
-    lines = reply.message.split("\p")
-    count = len(lines)
-    await edit_or_reply(event, f"⌔∮ عدد الأسطر في الرسالة: {count}")
+@l313l.on(events.NewMessage(pattern=r"\.الحماية تفعيل", outgoing=True))
+async def enable_protection(event):
+    global ban_time
+    ban_time = 60
+    await event.edit("تم تفعيل الحماية ضد المشرفين الذين يحظرون 5 أعضاء خلال 60 ثانية.")
+
+@l313l.on(events.NewMessage(pattern=r"\.الحماية اطفاء", outgoing=True))
+async def disable_protection(event):
+    global ban_time, banned_count
+    ban_time = 0
+    banned_count = {}
+    await event.edit("تم إطفاء الحماية ضد المشرفين.")
+
+@l313l.on(events.NewMessage(outgoing=True))
+async def monitor_banned_members(event):
+    if not isinstance(event.chat, types.Chat) or not event.chat.admin_rights:
+        return
+
+    if event.chat.admin_rights.ban_users:
+        admin_id = event.sender_id
+        chat_id = event.chat_id
+
+        if should_ban_admin(chat_id, admin_id):
+            await l313l.edit_permissions(chat_id, admin_id, ban_rights=types.ChatBannedRights())
+            await event.respond("تم حظر المشرف الذي قام بحظر 5 أعضاء خلال 60 ثانية.")
