3ab55b5b5b8f96210963f53646cb6bdb9f4bac6a
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-08-01 03:06:03 +0300

    Update يالحمايهه.py

--- SHRU/plugins/يالحمايهه.py
+++ SHRU/plugins/يالحمايهه.py
@@ -1,46 +1,53 @@
-import time
-from telethon.tl import types
+import asyncio
+from collections import defaultdict
 from telethon import events
+from telethon.tl.types import Channel, Chat
 from SHRU import l313l
-# Global variable to keep track of banned members count and time
-banned_count = {}
-ban_time = 60  # 60 seconds
 
-# Function to check if an admin should be banned
-def should_ban_admin(chat_id, admin_id):
-    if admin_id in banned_count:
-        current_time = time.time()
-        last_ban_time = banned_count[admin_id]
-        if current_time - last_ban_time <= ban_time:
-            return True
-        else:
-            banned_count[admin_id] = current_time
-    else:
-        banned_count[admin_id] = time.time()
-    return False
+banned_counts = defaultdict(int)
+
+# Time window in seconds to count bans
+BAN_TIME_WINDOW = 60
 
 @l313l.on(events.NewMessage(pattern=r"\.الحماية تفعيل", outgoing=True))
 async def enable_protection(event):
-    global ban_time
-    ban_time = 60
-    await event.edit("تم تفعيل الحماية ضد المشرفين الذين يحظرون 5 أعضاء خلال 60 ثانية.")
+    global banned_counts
+    await event.edit("تم تفعيل الحماية ضد الإداريين الذين يحظرون 5 أعضاء في 60 ثانية.")
+    banned_counts = defaultdict(int)
 
 @l313l.on(events.NewMessage(pattern=r"\.الحماية اطفاء", outgoing=True))
 async def disable_protection(event):
-    global ban_time, banned_count
-    ban_time = 0
-    banned_count = {}
-    await event.edit("تم إطفاء الحماية ضد المشرفين.")
+    global banned_counts
+    await event.edit("تم إطفاء الحماية ضد الإداريين.")
+    banned_counts = defaultdict(int)
 
-@l313l.on(events.NewMessage(outgoing=True))
-async def monitor_banned_members(event):
-    if not isinstance(event.chat, types.Chat) or not event.chat.admin_rights:
+@l313l.on(events.ChatAction())
+async def check_ban_count(event):
+    global banned_counts
+
+    if not isinstance(event.action, events.ChatActionBanned):
+        # Only count if it's a ban action
         return
 
-    if event.chat.admin_rights.ban_users:
-        admin_id = event.sender_id
-        chat_id = event.chat_id
+    chat_id = event.chat_id
+    user_id = event.user_id
 
-        if should_ban_admin(chat_id, admin_id):
-            await l313l.edit_permissions(chat_id, admin_id, ban_rights=types.ChatBannedRights())
-            await event.respond("تم حظر المشرف الذي قام بحظر 5 أعضاء خلال 60 ثانية.")
+    # Check if the user is an admin
+    chat = await event.get_chat()
+    if not isinstance(chat, (Channel, Chat)):
+        return
+    if not chat.admin_rights:
+        return
+
+    # Increment the ban count for the admin
+    banned_counts[(chat_id, user_id)] += 1
+
+    # Check if the admin reached the ban limit
+    if banned_counts[(chat_id, user_id)] >= 5:
+        # Ban the admin
+        try:
+            await event.client.kick_participant(chat_id, user_id)
+        except Exception as e:
+            print(f"Error banning admin: {e}")
+
+        # Reset the ban count
