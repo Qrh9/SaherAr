e0803298668a2de07a5d7da2511cbb2a5b48c1aa
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-08-03 00:16:20 +0300

    Update تجميع نقاط.py

--- SHRU/plugins/تجميع نقاط.py
+++ SHRU/plugins/تجميع نقاط.py
@@ -31,7 +31,7 @@ async def _(event):
         await asyncio.sleep(2)
         msg0 = await l313l.get_messages('@EEObot', limit=1)
         await msg0[0].click(2)
-        await asyncio.sleep(2)
+        await asyncio.sleep(0)
         msg1 = await l313l.get_messages('@EEObot', limit=1)
         await msg1[0].click(0)
 
@@ -39,7 +39,7 @@ async def _(event):
         for i in range(100):
             if SHRU[0] == 'no':
                 break
-            await asyncio.sleep(2)
+            await asyncio.sleep(0)
 
             list = await l313l(GetHistoryRequest(peer=channel_entity, limit=1, offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
             msgs = list.messages[0]
