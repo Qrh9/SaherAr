d04e1be23f0282a5e2b1fcef87df1f59caede0b9
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-08-21 03:07:42 +0300

    علاوي

--- SHRU/plugins/ميمز.py
+++ SHRU/plugins/ميمز.py
@@ -559,3 +559,9 @@ async def jepmeme(memejep):
   url = f"https://t.me/fasngonV/1264"
   await memejep.client.send_file(memejep.chat_id,url,caption="",parse_mode="html",reply_to=Jep)
   await memejep.delete()
+@l313l.on(admin_cmd(outgoing=True, pattern="علاوي$"))
+async def jepmeme(memejep):
+  Jep = await reply_id(memejep)
+  url = f"https://t.me/memesoundalsaher/4"
+  await memejep.client.send_file(memejep.chat_id,url,caption="",parse_mode="html",reply_to=Jep)
+  await memejep.delete()
