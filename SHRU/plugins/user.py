730ca0aaba626ea754a2d420adedd1aaea459d64
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-09-14 03:12:19 +0300

    Update user.py

--- SHRU/plugins/user.py
+++ SHRU/plugins/user.py
@@ -132,7 +132,7 @@ async def generate_random_usernames(event):
         v2 = ''.join((random.choice(abc) for _ in range(1)))
         v3 = ''.join((random.choice(abc1) for _ in range(1)))
         v4 = ''.join((random.choice(abc) for _ in range(1)))
-        username = f"{v1}{v1}{v1}{v3}{v1}{v1}"
+        username = f"{v1}{v1}{v3}{v1}{v1}{v1}"
         if not await Username_exists_by_Qrh9(username):
             generated_usernames.append(username)
             count -= 1
