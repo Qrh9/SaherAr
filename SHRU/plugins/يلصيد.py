059111c744a6f18a0025740319367ed8cacab55e
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-11-09 02:20:45 +0300

    Update ⁨يلصيد.py⁩

--- SHRU/plugins/يلصيد.py
+++ SHRU/plugins/يلصيد.py
@@ -152,44 +152,22 @@ async def generate_random_usernames(event):
 allowed_users = [5835316914]
 @Qrh9.on(events.NewMessage)
 async def handle_messages(event):
-     user_id = event.sender_id
-    if user_id in allowed_users:
-        message_text = event.message.text.strip()
-        if message_text == 'منصبين؟':
-            if user_id in allowed_users:
-                await event.respond(" ;)")
-@Qrh9.on(events.NewMessage)
-async def handle_messages(event):
-    user_id = event.sender_id
-    if user_id in allowed_users:
-        message_text = event.message.text.strip()
-        if message_text == 'منو فخر العرب؟':
-            if user_id in allowed_users:
-                await event.respond("الامام علي عليه السلام🤍")
-@Qrh9.on(events.NewMessage)
-async def handle_messages(event):
-    user_id = event.sender_id
-    if user_id in allowed_users:
-        message_text = event.message.text.strip()
-        if message_text == 'تحبوني؟':
-            if user_id in allowed_users:
-                await event.reply("نموت عليك سيد")
-@Qrh9.on(events.NewMessage)
-async def handle_messages(event):
-    user_id = event.sender_id
-    if user_id in allowed_users:
-        message_text = event.message.text.strip()
-        if message_text == 'شهر الحسين يا ناس':
-            if user_id in allowed_users:
-                await event.reply("ياا حسين 💔")
-@Qrh9.on(events.NewMessage)
-async def handle_messages(event):
     user_id = event.sender_id
     message_text = event.message.text.strip()
-    if user_id in allowed_users and message_text == 'يلا':
-        # Replace 'specific_id' with the ID you want to respond to.
-        specific_id =6051188407
-        
-        # Check if the message is from the specific user you want to respond to.
-        if user_id == specific_id:
-            await event.respond("كل عام وانت بخير سيد 🖤")
+
+    if user_id in allowed_users:
+        if message_text == 'منصبين؟':
+            await event.respond(" ;)")
+        elif message_text == 'منو فخر العرب؟':
+            await event.respond("الامام علي عليه السلام🤍")
+        elif message_text == 'تحبوني؟':
+            await event.reply("نموت عليك سيد")
+        elif message_text == 'شهر الحسين يا ناس':
+            await event.reply("ياا حسين 💔")
+        elif message_text == 'يلا':
+            # Replace 'specific_id' with the ID you want to respond to.
+            specific_id = 6051188407
+            
+            # Check if the message is from the specific user you want to respond to.
+            if user_id == specific_id:
+                await event.respond("كل عام وانت بخير سيد 🖤")
