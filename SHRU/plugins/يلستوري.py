b1e0ce3a955999580f32c2f858e5565bb2aab291
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2023-11-17 16:34:55 +0300

    Update ⁨يلستوري.py⁩

--- SHRU/plugins/يلستوري.py
+++ SHRU/plugins/يلستوري.py
@@ -1,7 +1,2 @@
-from telethon import types
-from telethon.tl.functions.stories import UploadMedia, UploadStory, GetStories
-from telethon.tl.types import InputMessagesFilterDocument, InputMessagesFilterPhoto
-import asyncio
-import os
-from SHRU import Qrh9
+
 
