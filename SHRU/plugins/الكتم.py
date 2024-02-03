bf42e9a2fc997003678438ed4535b8a3eb07e695
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-02-03 18:44:38 +0300

    Update ⁨الكتم.py⁩

--- SHRU/plugins/الكتم.py
+++ SHRU/plugins/الكتم.py
@@ -188,33 +188,34 @@ async def unmutejep(event):
             )
 
 
+from telethon.errors import MessageDeleteForbiddenError
+from telethon.events import NewMessage
+from telethon import events
+
 private_mode = {}
 
 @Qrh9.on(events.NewMessage(pattern=r"^\.تشغيل الخصوصيه$"))
-async def close_private(event):
+async def close_private(event: NewMessage.Event) -> None:
     chat_id = event.chat_id
     private_mode[chat_id] = True
     await event.reply("**وضع الخصوصيه مفعل الان محد يكدر يراسلك**")
 
 @Qrh9.on(events.NewMessage(pattern=r"^\.اطفاء الخصوصيه$"))
-async def open_private(event):
+async def open_private(event: NewMessage.Event) -> None:
     chat_id = event.chat_id
-    if chat_id in private_mode and private_mode[chat_id]:
-        del private_mode[chat_id]
+    if private_mode.pop(chat_id, False):
         await event.reply("**تم فتح الخاص بنجاح**")
     else:
         await event.reply("**الخاص مفتوح بالفعل**")
 
 @Qrh9.on(events.NewMessage)
-async def handle_message(event):
+async def handle_message(event: NewMessage.Event) -> None:
     sender_id = event.sender_id
-    if sender_id in private_mode and private_mode[sender_id]:
+    if private_mode.get(sender_id, False):
         try:
             await event.delete()
         except MessageDeleteForbiddenError:
-
             pass
-
 
 @Qrh9.ar_cmd(incoming=True)
 async def watcher(event):
