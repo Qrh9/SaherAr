0432c788ec3bf51e443dfca79f9d9bbae2c4a9e6
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-02-05 17:50:06 +0300

    Update ccg.py

--- SHRU/plugins/ccg.py
+++ SHRU/plugins/ccg.py
@@ -1,14 +1,16 @@
 from telethon import events
 from SHRU import Qrh9
-
-
+import html
+import os
+from SHRU.core.logger import logging
+from ..Config import Config
 
 @Qrh9.on(admin_cmd(pattern="(مميز؟)"))
 async def is_vip(event):
     username = event.message.sender.username
     user_id = event.message.sender_id
     
-    if username in vip_members or user_id in vip_members.values():
+    if user_id in Config.Vip_members.values():
         await event.respond('**هذا عضو مميز بالسورس 🫡**')
     else:
         await event.respond('**ليس عضو مميز**')
