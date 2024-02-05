483ddcc6c2530f201afd57833864c40210aa17ff
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-02-05 17:57:37 +0300

    Update ccg.py

--- SHRU/plugins/ccg.py
+++ SHRU/plugins/ccg.py
@@ -4,14 +4,11 @@ import html
 import os
 from SHRU.core.logger import logging
 from ..Config import Config
-
 @Qrh9.on(admin_cmd(pattern="(مميز؟)"))
 async def is_vip(event):
-    username = event.message.sender.username
     user_id = event.message.sender_id
     
     if user_id in Config.Vip_members():
         await event.respond('**هذا عضو مميز بالسورس 🫡**')
     else:
         await event.respond('**ليس عضو مميز**')
-
