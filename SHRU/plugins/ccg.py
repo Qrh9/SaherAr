fc1e3354f28a2ff4ca8baed6c7fbc6d18df48fd4
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-02-05 18:48:45 +0300

    Update ccg.py

--- SHRU/plugins/ccg.py
+++ SHRU/plugins/ccg.py
@@ -1,15 +1,3 @@
-from telethon import events
-from SHRU import Qrh9
-import html
-import os
-from SHRU.core.logger import logging
-from ..Config import Config
 
 
-@Qrh9.on(events.NewMessage(pattern="^مميز؟"))
-async def check_vip_membership(event):
-    user_id = replied_user.id
-    if user_id in Config.Vip_members:
-        await event.reply("نعم، أنت عضو مميز!")
-    else:
-        await event.reply("لا، أنت لست عضو مميز.")
+
