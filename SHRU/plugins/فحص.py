95c1abbfcc64b856eab369a0188c14a11b7d49fa
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-02-16 15:08:43 +0300

    Update ⁨فحص.py⁩

--- SHRU/plugins/فحص.py
+++ SHRU/plugins/فحص.py
@@ -5,7 +5,21 @@ import asyncio
 import os
 from datetime import datetime
 from platform import python_version
+from requests import get
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.utils import get_input_location
 
+from SHRU import Qrh9
+from random import choice
+from Qrh9.razan.resources.strings import *
+from telethon import events
+from ..Config import Config
+from ..core.managers import edit_or_reply
+from ..helpers import get_user_from_event, reply_id
+from . import spamwatch
+from telethon.utils import get_display_name
+from ..helpers.utils import reply_id, _catutils, parse_pre, yaml_format, install_pip, get_user_from_event, _format
 from telethon import version
 from telethon.errors.rpcerrorlist import (
     MediaEmptyError,
