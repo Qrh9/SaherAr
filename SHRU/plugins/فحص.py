01d2a6e56205e1e803e78e3cf511e4f1eb383d47
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-02-16 15:13:29 +0300

    Update ⁨فحص.py⁩

--- SHRU/plugins/فحص.py
+++ SHRU/plugins/فحص.py
@@ -37,7 +37,32 @@ from ..sql_helper.globals import gvarstatus
 from . import mention
  
 plugin_category = "utils"
-
+async def get_user_from_event(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_object = await event.client.get_entity(previous_message.sender_id)
+    else:
+        user = event.pattern_match.group(1)
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            self_user = await event.client.get_me()
+            user = self_user.id
+        if event.message.entities:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await event.client.get_entity(user_id)
+                return user_obj
+        if isinstance(user, int) or user.startswith("@"):
+            user_obj = await event.client.get_entity(user)
+            return user_obj
+        try:
+            user_object = await event.client.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+    return user_object
 #كتـابة وعـديل:  @ll1iltت
 file_path = "installation_date.txt"
 if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
