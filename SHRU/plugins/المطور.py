e8764086d957e96cb79fa1e63fad30b507c1a635
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-02-29 20:09:05 +0300

    Update ⁨المطور.py⁩

--- SHRU/plugins/المطور.py
+++ SHRU/plugins/المطور.py
@@ -47,8 +47,9 @@ async def amireallyalive(event):
         cat_caption = f"مطورين الساحر\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         cat_caption += f"- المطور الاساسي :@ll1ilt\n"
-        cat_caption += f"- المطور  :@مورفن\n"
+        cat_caption += f"- المطور  :@hq_nl\n"
         cat_caption += f"- المطور  :@Y7yuu\n"
+        cat_caption += f"- المطور  :@Xl5x5\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         await event.client.send_file(
             event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
