cc951626b99e78c97b3ec7b8ac1b7ba62fc49fcd
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-03-08 20:45:13 +0300

    Update ⁨المطور.py⁩

--- SHRU/plugins/المطور.py
+++ SHRU/plugins/المطور.py
@@ -61,7 +61,7 @@ async def on_plug_in_callback_query_handler(event):
     statstext = await catalive(StartTime)
     await event.answer(statstext, cache_time=0, alert=True)
 
-progs = in Config.Cash_Dev
+progs = Config.Cash_Dev
 
 @Qrh9.on(events.NewMessage(incoming=True))
 async def reda(event):
