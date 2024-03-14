4d34e1428e3dad0cec5ada360908b042bf5f215c
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-03-14 07:47:20 +0300

    Update ⁨يارمضان.py⁩

--- SHRU/plugins/يارمضان.py
+++ SHRU/plugins/يارمضان.py
@@ -127,4 +127,4 @@ async def emoji_race(event):
     race_end_time = datetime.now()
     time_taken = (race_end_time - race_start_time).total_seconds()
     winner = await Qrh9.get_entity(response.sender_id)
-    await edit_or_reply(event,f"🎉 مبروك {winner.first_name}! لقد فزت بالسباق في {time_taken} ثواني! لقد فزت وحصلت على نقطة!")
+    await message.reply(f"🎉 مبروك {winner.first_name}! لقد فزت بالسباق في {time_taken} ثواني! لقد فزت وحصلت على نقطة!")
