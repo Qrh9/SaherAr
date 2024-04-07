4479ebeca75d0be97469cb956e0d36b3fad1dc26
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-04-07 14:39:03 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,10 +3,10 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    APP_ID = 28658428
+    API_HASH = "26942dd8ca08c8eff6601a4f77ea5366"
     # the name to display in your alive message
-    ALIVE_NAME = "Your value"
+    ALIVE_NAME = "Alsaher Vps"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "Your value"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
