cdf81029417806135c9389738d6fea26793b8b1b
Qrh9 <gl1qmyoj@gmail.com>
2024-09-17 02:35:35 +0300

    Update aisoe.py

--- SHRU/plugins/aisoe.py
+++ SHRU/plugins/aisoe.py
@@ -1,1 +1,29 @@
-#
+from telethon import events
+from instaloader import Instaloader, Profile
+from SHRU import Qrh9
+from ..core.managers import edit_or_reply
+
+@Qrh9.on(events.NewMessage(pattern=r".كشانستا(?: |$)(.*)"))
+async def _int(event):
+    user1 = event.pattern_match.group(1)
+    if not user1:
+        return await edit_or_reply(event, "⌔∮ يرجى إدخال اسم مستخدم إنستغرام صالح.")
+    
+    loader = Instaloader()
+    profile = None
+
+    try:
+        profile = Profile.from_username(loader.context, user1)
+    except Exception as e:
+        return await edit_or_reply(event, f"⌔∮ خطأ: {str(e)}")
+
+    pr = f"""
+⌔∮ معلومات حساب إنستغرام:
+- **الاسم**: {profile.full_name}
+- **المتابعين**: {profile.followers}
+- **يتابع**: {profile.followees}
+- **عدد المنشورات**: {profile.mediacount}
+- **البايو**: {profile.biography}
+- **خاص/عام**: {"خاص" if profile.is_private else "عام"}
+"""
+    await edit_or_reply(event, pr)
