11a1846bf1fdb903b33874bc7e8fcf3fc5255846
Qrh9 <gl1qmyoj@gmail.com>
2024-09-17 05:41:48 +0300

    Update يلستوري.py

--- SHRU/plugins/يلستوري.py
+++ SHRU/plugins/يلستوري.py
@@ -2,7 +2,7 @@ import random
 from telethon import events
 from SHRU import Qrh9
 from ..core.managers import edit_or_reply
-from datetime import datetime
+from datetime import datetime, timedelta
 
 user_join_times = {}
 fshr_active = {}
@@ -58,8 +58,54 @@ async def check_time(event):
         days, seconds = time_diff.days, time_diff.seconds
         hours = seconds // 3600
         minutes = (seconds % 3600) // 60
-        response = f"⌔∮ صارلك متواجد هنا لمدة: {days} يوم، {hours} ساعة، و {minutes} دقيقة."
+        response = f"⌔∮ صارلك متواجد هنا لمدة: {days} يوم، {hours} ساعة، و {minutes} دقيقة"
     else:
-        response = "⌔∮ ما اكدر اعرف شكد صارلك داخل، يمكن دخلت قبل لا نبدي نتبع التواجد."
+        response = "⌔∮ ما اكدر اعرف شكد صارلك داخل، يمكن دخلت قبل لا نبدي نتبع التواجد"
 
     await edit_or_reply(event, response)
+
+
+afk = {}
+
+@Qrh9.on(events.NewMessage(pattern=r".غياب (\d+)([smhd])(?: |$)(.*)"))
+async def set_afk(event):
+    duration_value = int(event.pattern_match.group(1))
+    duration_type = event.pattern_match.group(2)
+    reason = event.pattern_match.group(3) or "غير معروف"
+
+    if duration_type == 's':
+        duration = timedelta(seconds=duration_value)
+    elif duration_type == 'm':
+        duration = timedelta(minutes=duration_value)
+    elif duration_type == 'h':
+        duration = timedelta(hours=duration_value)
+    elif duration_type == 'd':
+        duration = timedelta(days=duration_value)
+
+    afk_until = datetime.now() + duration
+
+    afk[event.sender_id] = {
+        "until": afk_until,
+        "reason": reason
+    }
+
+    await edit_or_reply(event, f"⌔∮ تم تفعيل الغياب لمدة {duration_value}{duration_type} السبب: {reason}")
+
+@Qrh9.on(events.NewMessage(pattern=r".رجعت"))
+async def remove_afk(event):
+    if event.sender_id in afk:
+        del afk[event.sender_id]
+        await edit_or_reply(event, "⌔∮تم الالغاء منور")
+    else:
+        await edit_or_reply(event, "⌔∮ هله بيك؟...")
+
+@Qrh9.on(events.NewMessage())
+async def auto_reply_afk(event):
+    if event.sender_id in afk:
+        afk_info = afk[event.sender_id]
+        if datetime.now() < afk_info['until']:
+            remaining_time = afk_info['until'] - datetime.now()
+            await event.reply(f"⌔∮ المستخدم غايب لفتره السبب: {afk_info['reason']}. باقي: {remaining_time}")
+        else:
+            del afk[event.sender_id]
+            await event.reply("⌔∮المستخدم رجع تكدر تراسله هسه")
