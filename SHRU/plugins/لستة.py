dc882f0ba1af280c1b8bf1f2c397f366608a1c52
Qrh9 <gl1qmyoj@gmail.com>
2024-09-17 04:37:43 +0300

    Update لستة.py

--- SHRU/plugins/لستة.py
+++ SHRU/plugins/لستة.py
@@ -1,39 +1,44 @@
-# Copyright (C) 2021 SHRU TEAM
-# FILES WRITTEN BY  @ll1ilt
-import os
-import re
+from telethon import events
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.messages import ForwardMessagesRequest
+from SHRU import Qrh9
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from SHRU.utils import admin_cmd
 
-from telethon import Button
+riochannel = {}
 
-from ..Config import Config
-from . import Qrh9, edit_delete, reply_id
+@Qrh9.on(admin_cmd(pattern=r".تقليد_قناة(?: |$)(.*)"))
+async def clone_channel(event):
+    channel_username = event.pattern_match.group(1)
+    if not channel_username:
+        return await event.edit("⌔∮ يرجى تحديد اسم مستخدم القناة لتقليدها.")
+    
+    try:
+        await event.client(JoinChannelRequest(channel_username))
+    except Exception as e:
+        return await event.edit(f"⌔∮ حدث خطأ أثناء الانضمام للقناة: {str(e)}")
+    
+    riochannel[channel_username] = event.chat_id
+    await event.edit(f"⌔∮ تم تقليد القناة: {channel_username}.\nأي منشور جديد سيتم نشره في قناتك.")
 
-plugin_category = "tools"
-BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>)")
+@Qrh9.on(admin_cmd(pattern=r".حذف_التقليد(?: |$)(.*)"))
+async def remove_cloning(event):
+    channel_username = event.pattern_match.group(1)
+    if not channel_username or channel_username not in riochannel:
+        return await event.edit("⌔∮ يرجى تحديد قناة قمت بتقليدها لحذف التقليد.")
+    
+    del riochannel[channel_username]
+    await event.edit(f"⌔∮ تم حذف تقليد القناة: {channel_username}.")
 
-@Qrh9.ar_cmd(pattern="لستة$")
-async def _(event):
-    "To create button posts via inline"
-    reply_to_id = await reply_id(event)
-    # soon will try to add media support
-    reply_message = await event.get_reply_message()
-    if reply_message:
-        markdown_note = reply_message.text
-    else:
-        markdown_note = "".join(event.text.split(maxsplit=1)[1:])
-    if not markdown_note:
-        return await edit_delete(event, "᯽︙ يجب عليك وضع مسافـة لاستخدامها مع الامر ")
-    catinput = "Inline buttons " + markdown_note
-    results = await event.client.inline_query(Config.TG_BOT_USERNAME, catinput)
-    await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
-    await event.delete()
-
-
-def build_keyboard(buttons):
-    keyb = []
-    for btn in buttons:
-        if btn[2] and keyb:
-            keyb[-1].append(Button.url(btn[0], btn[1]))
-        else:
-            keyb.append([Button.url(btn[0], btn[1])])
-    return keyb
+@Qrh9.on(events.NewMessage(chats=riochannel.keys()))
+async def forward_to_channel(event):
+    target_channel_id = riochannel.get(event.chat.username)
+    if target_channel_id:
+        try:
+            await Qrh9(ForwardMessagesRequest(
+                from_peer=event.chat_id,
+                id=[event.id],
+                to_peer=target_channel_id
+            ))
+        except Exception as e:
+            await event.client.send_message(target_channel_id, f"⌔∮ خطأ أثناء تقليد المنشور: {str(e)}")
