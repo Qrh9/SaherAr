b6f1dcd6ba780f59b8c1131f6ccf2fcdcff7d5f7
Qrh9 <112596005+Qrh9@users.noreply.github.com>
2024-09-23 22:27:18 +0300

    Update تلكراف.py

--- SHRU/plugins/تلكراف.py
+++ SHRU/plugins/تلكراف.py
@@ -27,12 +27,10 @@ def resize_image(image):
     command=("تلكراف", plugin_category),
     info={
         "header": "To get telegraph link.",
-        "description": "Reply to text message to paste that text on telegraph, or reply to media to upload to Telegraph (max 5MB)",
+        "description": "Reply to text message to paste that text on telegraph you can also pass input along with command.",
         "usage": [
             "{tr}tgm",
-            "{tr}tgt <optional_title>",
-            "{tr}telegraph media",
-            "{tr}telegraph text <optional_title>",
+            "{tr}tgt <title(optional)>",
         ],
     },
 )
@@ -49,17 +47,18 @@ async def _(event):
 
     if input_str in ["ميديا", "m"]:
         downloaded_file_name = await event.client.download_media(r_message, Config.TEMP_DIR)
-        await jmevent.edit(f"⌔︙تـم التحـميل إلى {downloaded_file_name}")
+        await jmevent.edit(f"⌔︙تـم التحـميل الـى {downloaded_file_name}")
 
         if downloaded_file_name.endswith((".webp")):
             resize_image(downloaded_file_name)
 
         try:
             media_urls = upload_file(downloaded_file_name)
+            
             if isinstance(media_urls, list) and len(media_urls) > 0:
                 media_url = media_urls[0]
             else:
-                raise ValueError("الاستجابة من Telegraph غير صحيحة.")
+                raise ValueError(f"الاستجابة غير صحيحة من Telegraph: {media_urls}")
         except exceptions.TelegraphException as exc:
             await jmevent.edit(f"⌔︙خـطأ : \n`{exc}`")
             os.remove(downloaded_file_name)
@@ -71,32 +70,3 @@ async def _(event):
                 f"⌔︙الـرابـط : [اضغط هنا](https://telegra.ph{media_url})\n⌔︙الوقـت المستغرق : `{ms} ثـواني.`",
                 link_preview=False,
             )
-
-    elif input_str in ["نص", "t"]:
-        user_object = await event.client.get_entity(r_message.sender_id)
-        title_of_page = get_display_name(user_object)
-
-        if optional_title:
-            title_of_page = optional_title
-
-        page_content = r_message.message
-        if r_message.media:
-            downloaded_file_name = await event.client.download_media(r_message, Config.TEMP_DIR)
-            with open(downloaded_file_name, "rb") as fd:
-                page_content += fd.read().decode("UTF-8") + "\n"
-            os.remove(downloaded_file_name)
-
-        page_content = page_content.replace("\n", "<br>")
-        try:
-            response = telegraph.create_page(title_of_page, html_content=page_content)
-        except Exception as e:
-            LOGS.error(e)
-            title_of_page = "".join(random.choice(string.ascii_letters) for _ in range(16))
-            response = telegraph.create_page(title_of_page, html_content=page_content)
-
-        end = datetime.now()
-        ms = (end - start).seconds
-        await jmevent.edit(
-            f"⌔︙الـرابـط : [اضغط هنا](https://telegra.ph/{response['path']})\n⌔︙الوقـت المستغرق : `{ms} ثـواني.`",
-            link_preview=False,
-        )
