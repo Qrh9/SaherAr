62f83f23dd757baa556a7aca14ade0f4539e0e4f
Qrh9 <gl1qmyoj@gmail.com>
2024-10-04 14:02:04 +0300

    o

--- stringsetup.py
+++ stringsetup.py
@@ -2,7 +2,7 @@
 # (c) https://t.me/TelethonChat/37677
 # This Source Code Form is subject to the terms of the GNU
 # General Public License, v.3.0. If a copy of the GPL was not distributed with this
-# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.#
+# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.#o
 
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
